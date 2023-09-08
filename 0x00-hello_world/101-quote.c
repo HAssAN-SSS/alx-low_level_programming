cb2d3cb999da1266fb027a02bd4d2d5f81da7c71
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-08 03:05:44 +0100

    i can do it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,12 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- **/
+ */
 int main(void)
 {
-char *message = "and that piece of art is us
-eful\" - Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art
+is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(2, message, 59);
-
 return (1);
 }
