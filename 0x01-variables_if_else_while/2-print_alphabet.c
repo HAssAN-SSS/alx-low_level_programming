35b75bcc865ebb3edaf3169c91f455c0ad3ec119
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-13 20:16:52 +0100

    my hart is broken

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,8 +9,9 @@
  */
 int main(void)
 {
+    int i = 0;
 	char *string = "abcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < 26; i++)
+    for (i = 0; i < 26; i++)
     {
     putchar(string[i]);
     }
