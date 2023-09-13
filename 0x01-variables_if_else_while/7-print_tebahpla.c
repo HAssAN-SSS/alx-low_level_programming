ca2ee8c216e97769ba5f589d8f134f4a82bdb8d2
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-13 20:45:37 +0100

    my hart is broken

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 26; i <= 9; i--)
+{
+putchar('z' + i);
+}
+putchar('\n');
+
+	return (0);
+}
