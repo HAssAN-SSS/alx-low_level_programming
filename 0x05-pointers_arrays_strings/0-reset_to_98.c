aeb1903eebcab73ce183b67c538581eacd4e427d
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-18 22:11:32 +0100

    never return back

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
-/*
-* reset_to_98 - check the code
-* reset_to_98
-* reset_to_98
-* reset_to_98
-* reset_to_98
-* reset_to_98
-* Return: Always 0.
-*/
 
-void reset_to_98(int *n)
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98.
+ * @n: A pointer to an integer whose value will be set to 98.
+ *
+ * Description: This function takes a pointer to an integer as a parameter
+ * and updates the value stored at the memory location pointed to by the
+ * pointer to be 98.
+ */void reset_to_98(int *n)
 {
 *n = 98;
 }
