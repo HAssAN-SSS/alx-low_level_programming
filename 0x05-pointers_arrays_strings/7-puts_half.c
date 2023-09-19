b948d888fd0c51c64b4258bd5c52104c9ea536e9
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 19:57:00 +0100

    adelante

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* puts_half - Print a string in reverse followed by a newline.
+* @str: The input string to be printed in reverse.
+*/
+void puts_half(char *str)
+{
+int length = 0;
+int i;
+
+
+while (str[length] != '\0')
+{
+length++;
+}
+
+if (length % 2 == 0)
+{
+for (i = length / 2; i < length; i++)
+{
+putc(str[i], stdout);
+}
+}
+else
+{
+for (i = (length - 1) / 2; i < length - 1; i++)
+{
+putc(str[i], stdout);
+}
+}
+putc('\n', stdout);
+}
