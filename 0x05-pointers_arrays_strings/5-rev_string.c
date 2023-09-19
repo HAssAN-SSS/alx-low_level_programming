314619b4bda952deb72addef96c3017e893b35f8
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 03:36:45 +0100

    never return back

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 /**
- * rev_string - Print a string in reverse followed by a newline.
- * @str: The input string to be printed in reverse.
- */
+* rev_string - Print a string in reverse followed by a newline.
+* @str: The input string to be printed in reverse.
+*/
 void rev_string(const char *str)
 {
 int length = 0;
-int i;
-int start = 0;
-int end = length - 1;
-char temp;
 
+int start = 0;
+
+int end;
+
+char temp;
 
 while (str[length] != '\0')
 {
 length++;
 }
 
-
+end = length - 1;
 
 while (start < end)
 {
-temp = (*str + start);
-(*str + start) = (*str + end);
-(*str + end) = temp;
+temp = str[start];
+str[start] = str[end];
+str[end] = temp;
 
 start++;
 end--;
 }
 }
+
 
 
 
