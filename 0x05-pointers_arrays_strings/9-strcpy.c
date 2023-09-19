a6eeee376fdcb79e606271c0d4cf1a5e56d78ac4
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 22:15:27 +0100

    adelante

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * _strcpy - Print elements of an integer array from index n to the end.
- * strcpy - Print elements of an integer array from index n to the end.
- * _strcpy - Print elements of an integer array from index n to the end.
- * _strcpy - Print elements of an integer array from index n to the end.
+ * _strcpy - Copy a string from source to destination.
+ * @dest: The destination buffer.
+ * @src: The source string to be copied.
+ * Return: A pointer to the destination buffer.
  */
 
 char _strcpy(char *dest, char *src)
