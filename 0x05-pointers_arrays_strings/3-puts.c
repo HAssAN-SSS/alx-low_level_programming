43cbe942346bf5e83c66250c859a31fcc7627ef8
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 03:01:54 +0100

    never return back

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
 /**
  * _puts - Print a string followed by a new line to stdout.
  * @str: The input string to be printed.
  */
 void _puts(const char *str)
 {
-printf("%s\n", str);
+ssize_t count = 0;
+
+
+while (str[count] != '\0')
+{
+
+if (write(1, &str[count], 1) == -1)
+{
+return -1;
+}
+count++;
+}
+
+if (write(1, "\n", 1) == -1)
+{
+return (-1);
+}
+return (count);
 }
