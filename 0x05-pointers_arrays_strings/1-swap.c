2124abb38c6472307c1af8850211995b34dbab53
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 02:43:31 +0100

    never return back

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+
+/**
+* swap_int - Swaps the values of two integers using pointers.
+* @a: A pointer to the first integer.
+* @b: A pointer to the second integer.
+*
+* Description: This function takes two pointers to integers as parameters
+* and swaps the values stored at the
+* memory locations pointed to by the pointers.
+*/
+void swap_int(int *a, int *b)
+{
+int *n;
+
+*n = *a;
+*a = *b;
+*b = *n
+}
