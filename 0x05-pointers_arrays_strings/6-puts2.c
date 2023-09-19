66e659f8748aaf9be65392d0599d41c9cbed182f
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 19:24:15 +0100

    adelante

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* puts2 - Print a string in reverse followed by a newline.
+* @str: The input string to be printed in reverse.
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+
+putc(str[i]);
+i++;
+
+
+}
+}
