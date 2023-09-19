80e4fbf2e50043e4217411acf0abab793499ec32
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 20:11:35 +0100

    adelante

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* print_array - Print a string in reverse followed by a newline.
+* @a: The input string to be printed in reverse.
+* @n: The input string to be printed in reverse.
+*/
+
+void print_array(int *a, int n)
+{
+int length = 0;
+int i;
+
+
+while (str[length] != '\0')
+{
+length++;
+}
+
+for (i = n; i <= length - 1; i++)
+{
+putchar(a[i]);
+}
+putchar('\n');
+}
