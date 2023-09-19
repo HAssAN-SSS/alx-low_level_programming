2110fc03c58a4068251f30f6ce53f6c9d71c74b7
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-19 22:13:37 +0100

    adelante

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _strcpy - Print elements of an integer array from index n to the end.
+ * strcpy - Print elements of an integer array from index n to the end.
+ * _strcpy - Print elements of an integer array from index n to the end.
+ * _strcpy - Print elements of an integer array from index n to the end.
+ */
+
+char _strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+
+}
