f046cad0c083fea5c266a181ccbda5e2bc7880ab
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-21 17:52:50 +0100

    dont watch news it sso bad

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncpy - Copy a string from source to destination.
+ * @dest: The destination buffer.
+ * @src: The source string to be copied.
+ * @n: The number of bytes to be copied.
+ * Return: A pointer to the destination buffer.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
