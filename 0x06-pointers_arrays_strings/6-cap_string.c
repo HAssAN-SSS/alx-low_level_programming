264686db4ef35153e972215e7c6e0b618bd7c762
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-21 21:58:16 +0100

    dont watch news it sso bad

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *cap_string - Compare two strings.
+ * @str:  is the number of elements of the array.
+ * @*:  is the number of elements of the array.
+ * Return: The difference between the two strings.
+ */
+
+char *cap_string(char *str)
+{
+char *str;
+char stringo[] = "life is so, short";
+int flag = 0;
+
+str = stringo;
+
+while (*str != '\0')
+{
+if (*str < '0')
+{
+flag = 1;
+
+}
+if (*str >= 'a' && *str <= 'z' && flag == 2)
+{
+            
+*str = *str - ('a' - 'A');
+flag = 0;
+}
+str++;
+if (flag == 1)
+{
+flag = 2;
+}
+else
+{
+flag = 0;
+}
+}
+
+
+return (str);
+}
