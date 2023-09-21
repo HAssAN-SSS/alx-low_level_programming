75f875c1f3213809fa72487957bd586348f36ec1
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-21 17:43:22 +0100

    dont watch news it sso bad

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The string to be appended to.
+ * @src: The string to be appended.
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
