3d21cbeb645f02a07d220cc18ffa9be92a930680
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-21 17:56:15 +0100

    dont watch news it sso bad

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - Compare two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * Return: The difference between the two strings.
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
