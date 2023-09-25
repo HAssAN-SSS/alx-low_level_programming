9e45b2713e28ae3a278396c6d74d6132b9b5b424
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-25 21:02:54 +0100

    let go hassan let go

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strpbrk - gets the length of a prefix substring
+ * @*s: string
+ * @accept: string
+ *
+ * Return: number of bytes in the initial segment of s
+ */
+
+char *_strpbrk(char *s, char *accept){
+
+char *result;
+result = strpbrk(s, accept);
+return (result);
+}
