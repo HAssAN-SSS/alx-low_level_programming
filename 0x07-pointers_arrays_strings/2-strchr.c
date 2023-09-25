5d01bd43dd3ae191c25322265ecedf245e6a71db
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-25 20:15:38 +0100

    let go hassan let go

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,14 @@
 #include <string.h>
 #include <stdio.h>
+
 /**
  * _strchr - locate character in string
- *  - Returns a pointer to the first occurrence
- * of the character c in the string s,
- * or NULL if the character is not found
- *@s: string
- *@c: character
+ * @s: string
+ * @c: character
+ *
+ * Return: pointer to dest
  */
+
 char *_strchr(char *s, char c)
 {
 char *result;
