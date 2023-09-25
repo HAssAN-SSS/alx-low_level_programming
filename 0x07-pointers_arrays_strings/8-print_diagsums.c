01aa4df5fa483535129657cb8bd754ad2a4709d5
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-25 22:13:21 +0100

    let go hassan let go

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
  * print_diagsums - prints the sum of the two diagonals of a matrix
@@ -19,5 +18,4 @@ sum1 += a[i * size + i];
 sum2 += a[i * size + (size - 1 - i)];
 }
 printf("%d, %d\n", sum1, sum2);
-
 }
