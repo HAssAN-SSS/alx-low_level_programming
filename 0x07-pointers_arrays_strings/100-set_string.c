146603eb344543dfe93f1a1d9f41e71be5cde9a8
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-25 22:47:48 +0100

    let go hassan let go

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * set_string - copies a string
+ * @s: destination
+ * @to: source
+ * Return: pointer to dest
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
