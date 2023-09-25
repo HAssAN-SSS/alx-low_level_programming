c4f411b9b55caa25d227db12c627c122ef9d6e1b
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-25 22:23:32 +0100

    let go hassan let go

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,9 +17,5 @@ for (i = 0; i < size; i++)
 sum1 += a[i * size + i];
 sum2 += a[i * size + (size - 1 - i)];
 }
-putchar(sum1 + '0');
-putchar(',');
-putchar(' ');
-putchar(sum2 + '0');
-putchar('\n');
+printf("%d, %d\n", sum1, sum2);
 }
