822a1e0e39c5ec83d71fe5777a782b0b8c33c220
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-25 21:10:45 +0100

    let go hassan let go

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search
+ * @needle: substring to search for
+ *
+ * Return: pointer to the beginning of the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+char *result;
+result = strstr(haystack, needle);
+}
