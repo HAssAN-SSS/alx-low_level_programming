6676e88e6da5f422817df1fe1185bb5f9871f581
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-30 21:39:21 +0100

    recursio

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * is_prime_number - returns the factorial of a given number
+ * @n: number to find the factorial of
+ * Return: factorial of n
+ */
+int is_prime_number(int n)
+{
+if (n % 2 == 0)
+{
+return (1);
+
+}
+else
+{
+return (0);
+}
+}
+
