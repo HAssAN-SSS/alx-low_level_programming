5989b0e233399dc931cfa1a4ecc02d01b04ffdb5
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-30 19:54:46 +0100

    recursio

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _sqrt_recursion - returns the factorial of a given number
+ * @n: number to find the factorial of
+ * Return: factorial of n
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+if (n == 0)
+{
+return (0);
+}
+else
+{
+if (n == 1)
+{
+return (1);
+}
+else
+{
+return (_sqrt_recursion(n / 2));
+}
+}
+}
+}
