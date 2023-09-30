b62894b8ebd9e473d1f7619a5630de88507636b5
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-30 19:26:40 +0100

    recursio

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,6 +8,13 @@
  */
 int _pow_recursion(int x, int y)
 {
+if (y == 0)
+{
+return (1);
+}
+else
+{
+
 if (y < 0)
 {
 return (-1);
@@ -22,6 +29,7 @@ else
 {
 
 return (x * _pow_recursion(x, y - 1));
+}
 }
 }
 }
