ae2062518cad9fe852309fe1820a78319bfef1de
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-30 19:20:50 +0100

    recursio

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _pow_recursion - returns the factorial of a given number
+ * @x: number to find the factorial of
+ * @y: number to find the factorial of
+ * Return: factorial of n
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
