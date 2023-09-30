6b48a4e2a76369153aa5796493b83b63cc3144a6
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-30 21:21:33 +0100

    recursio

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,20 +14,7 @@ return (-1);
 }
 else
 {
-if (n == 0)
-{
-return (0);
-}
-else
-{
-if (n == 1)
-{
-return (1);
-}
-else
-{
-return (_sqrt_recursion(n / n));
-}
-}
+
+return (_sqrt_recursion(n / 2));
 }
 }
