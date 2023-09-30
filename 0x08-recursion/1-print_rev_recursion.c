a8dfb277246e10d9ea8691c09b6745390cdebd4c
HAssAN-SSS <cruzacometas@gmail.com>
2023-09-30 18:56:02 +0100

    recursio

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * _puts_recursion - prints a string
+ * _print_rev_recursion - prints a string
  * @s: string
  * Return: void
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
 {
 return;
 }
-_puts_recursion(s);
+_print_rev_recursion(s);
 s++;
 }
