15f2000df50a4f88b42905bc52f5b564159d07ee
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-03 17:57:29 +0100

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+*/
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+printf("%s\n", argc - 1);
+return (0);
+}
