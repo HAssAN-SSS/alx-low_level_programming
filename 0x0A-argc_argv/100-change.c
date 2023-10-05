d1ca0d0bdd3b6bc475fdf9a98840580a662654e8
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-05 14:06:52 +0100

    coins coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,20 +8,23 @@
 */
 int main(int argc, char **argv)
 {
+int amount;
+int coins;
+int count;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-int amount = atoi(argv[1]);
+amount = atoi(argv[1]);
 if (amount < 0)
 {
 printf("0\n");
 }
 else
 {
-int coins[] = {25, 10, 5, 2, 1};
-int count = 0;
+coins[] = {25, 10, 5, 2, 1};
+count = 0;
 for (int i = 0; amount > 0 && i < 5; i++)
 {
 count += amount / coins[i];
