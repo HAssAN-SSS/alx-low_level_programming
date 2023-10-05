d8cd3cc5f8f5fc395a4360b05ef87d96ba806648
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-05 12:46:48 +0100

    the daily

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,3 @@
 #let create a bibliotic#
+
+## the static libraby is a sum of files in object stat of compilation and we use gcc whit c flag to stop in the linking stat, and we use ar -rc falgs to create oure librares files we can add -s flag to arr command to add a indix##
--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
-
-
 /**
  * main - prints the name of the program
- * displayError - prints the error if it exists
- * @argc: argument count
- * @argv: argument vector
- *
- * This function is responsible for printing the error if it exists.
- *
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
 */
-
-
 int main(int argc, char **argv)
 {
 int err = 0;
@@ -25,28 +13,22 @@ int alternativ = 0;
 int monidaCount = 0;
 int monidaIndex = 0;
 int array[5] = {25, 10, 5, 2, 1};
-
 if (argc != 2)
 {
 printf("Error\n");
 err = 1;
 }
-
 if (err != 1)
 {
 if (atoi(argv[1]) < 0)
 {
-printf("0");
-printf("\n");
+printf("0\n");
 }
 else
 {
-
 while (alternativ < atoi(argv[1]))
 {
-
 alternativ += array[monidaIndex];
-
 if (alternativ > atoi(argv[1]))
 {
 alternativ -= array[monidaIndex];
@@ -60,20 +42,9 @@ if (alternativ == atoi(argv[1]))
 printf("%d", monidaCount);
 printf("\n");
 }
-    
 }
-
-
-
-
-
 }
-
-
 }
-
 }
-
-
 return (err);
 }
