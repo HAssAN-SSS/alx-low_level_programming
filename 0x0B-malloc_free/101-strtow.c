952c127b5769f4e884a333f43f7b8ab90bc0c317
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-05 20:43:45 +0100

    El rey del pointers

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,86 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+
+int wordCount(char *str);
+
+/**
+ * wordCount - counts the number of words in a string
+ * @str: The string to be counted.
+ * Return: The number of words
+ */
+int wordCount(char *str)
+{
+int i;
+int count = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ')
+{
+count++;
+}
+}
+
+return (count + 1);
+}
+
 /**
  * strtow - splits a string into words.
  * @str: The string to be split.
  * Return: A pointer to an array
-*/
+ */
 char **strtow(char *str)
 {
+int i, j, k;
 char **father;
-int length = strlen(str);
-father = malloc(length + 1 * sizeof(char *));
-printf("%d", lenght);
-return ();
+int sizeCount = 0;
+int wordcount = wordCount(str);
+
+father = malloc((wordcount + 1) * sizeof(char *));
+if (father == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < wordcount; i++)
+{
+sizeCount = 0;
+while (str[sizeCount] != ' ' && str[sizeCount] != '\0')
+{
+sizeCount++;
+}
+father[i] = malloc((sizeCount + 1) * sizeof(char));
+if (father[i] == NULL)
+{
+for (k = 0; k < i; k++)
+{
+free(father[k]);
+}
+free(father);
+return (NULL);
 }
 
-/**
- * print_tab - Prints an array of string
- * @tab: The array to print
- *
- * Return: nothing
- */
-void print_tab(char **tab)
+for (j = 0; j < sizeCount; j++)
 {
-int i;
-
-for (i = 0; tab[i] != NULL; ++i)
-{
-printf("%s\n", tab[i]);
-}
+father[i][j] = str[j];
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
-int main(void)
+father[i][j] = '\0';
+
+str += sizeCount;
+
+while (*str == ' ')
 {
-char **tab;
-tab = strtow("      ALX School         #cisfun      ");
-if (tab == NULL)
-{
-printf("Failed\n");
-return (1);
+str++;
 }
-print_tab(tab);
-return (0);
 }
+father[wordcount] = NULL;
+}
+
+return (father);
+}
+
+
