66b36aed6b81347e3670de0029d609cde8c8ff1e
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-08 16:32:05 +0100

    king of malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -60,18 +60,5 @@ chunk[start + i] = s[i];
 }
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-char *concat;
 
-concat = string_nconcat("Best ", "School !!!", 6);
-printf("%s\n", concat);
-free(concat);
-return (0);
-}
 
