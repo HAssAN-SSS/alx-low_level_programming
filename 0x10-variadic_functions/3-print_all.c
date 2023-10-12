d09c50efac16fb0e49d24aff4a3e6184291e5571
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-12 21:17:50 +0100

    variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,14 +5,13 @@
  * @...: arguments
  * Returns: Nothing
 */
-
-
 void print_all(const char * const format, ...)
 {
 va_list args;
 va_start(args, format);
 unsigned int n = strlen(format);
 unsigned int i = 0;
+char *str = NULL;
 while (i < n)
 {
 switch (format[i])
@@ -20,19 +19,21 @@ switch (format[i])
 case 'c':
 printf("%c", va_arg(args, char));
 break;
-
 case 'i':
 printf("%d", va_arg(args, int));
 break;
-
 case 'f':
 printf("%d", va_arg(args, float));
 break;
-
 case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+{
+printf("(nil)");
+break;
+}
 printf("%s", va_arg(args, char *));
 break;
-
 default:
 break;
 }
@@ -42,4 +43,5 @@ if (i != n)
 printf(", ");
 }
 }
+printf("\n");
 }
