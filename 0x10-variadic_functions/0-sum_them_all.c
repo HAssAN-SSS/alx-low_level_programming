8a046a09025090d18ece80ac7d4c2638c0037f77
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-12 19:48:07 +0100

    variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters
+ * @n: The number of parameters passed to the function
+ * @...: A variable number of parameters to calculate the sum of
+ * Return: The sum of all parameters
+*/
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+int sum = 0;
+int i = 0;
+va_list args;
+if (n == 0)
+{
+return (0);
+}
+else
+{
+while (i < n)
+{
+sum += va_arg(args, int);
+i++;
+}
+return (sum);
+}
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+#variadic ohhh variadic#
