b343233100c589ce271d6a504d30c949891c84ec
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-12 20:09:05 +0100

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
-#include "variadic_functions.h"
+#include <stdio.h>
+
 
 /**
  * sum_them_all - Returns the sum of all its parameters
@@ -12,7 +13,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 int sum = 0;
-int i = 0;
+unsigned int i = 0;
 va_list args;
 if (n == 0)
 {
@@ -28,4 +29,19 @@ i++;
 }
 return (sum);
 }
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int sum;
+
+sum = sum_them_all(2, 98, 1024);
+printf("%d\n", sum);
+sum = sum_them_all(4, 98, 1024, 402, -1024);
+printf("%d\n", sum);
+return (0);
 }
