bfcce869d542491139ec074bebf1bbe88f423b4a
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-12 23:41:27 +0100

    variadic

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,17 +1,9 @@
 #include "function_pointers.h"
-/**
- * int_index - searches for an integer
- * @array: the array to search
- * @size: the size of the array
- * @cmp: the comparison function
- * Return: the index of the first element for which the cmp function
-*/
 
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
-int work = 0;
 if (size <= 0)
 {
 return (-1);
@@ -23,7 +15,6 @@ for (i = 0; i < size; i++)
 if (cmp(array[i]))
 {
 return (i);
-work = 1;
 }
 }
 }
@@ -31,4 +22,57 @@ work = 1;
 return (-1);
 
 
+}
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_98(int elem)
+{
+    return (98 == elem);
+}
+
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_strictly_positive(int elem)
+{
+    return (elem > 0);
+}
+
+
+/**
+ * abs_is_98 - check if the absolute value of a number is 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int abs_is_98(int elem)
+{
+    return (elem == 98 || -elem == 98);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+    int index;
+
+    index = int_index(array, 20, is_98);
+    printf("%d\n", index);
+    index = int_index(array, 20, abs_is_98);
+    printf("%d\n", index);
+    index = int_index(array, 20, is_strictly_positive);
+    printf("%d\n", index);
+    return (0);
 }
